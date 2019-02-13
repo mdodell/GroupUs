bbb262a614adeb70124325be06dc43387157bc52
mdodell <dodellmitchell@gmail.com>
2019-02-13 00:19:02 -0500

    Set it up so it now currUser reducer is null if no user is logged in, or contains a user object if the user is logged in.

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -12,7 +12,7 @@ export const receiveUser = (user) => {
 export const fetchUser = () => {
     return dispatch => {
         return axios.get('http://localhost:3001/auth/getUser',{headers: {"Access-Control-Allow-Origin": "http://localhost:3001"}, withCredentials: true})
-            .then(json => dispatch(receiveUser(json)));
+            .then(json => dispatch(receiveUser(json.data)));
     };
 };
 
